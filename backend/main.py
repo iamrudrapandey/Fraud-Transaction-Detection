e25c893428a7b1115728a91092fb360f8b7ba1cd
Rudra Pandey <rudrapandey2207@gmail.com>
2026-08-30 21:29:59 +0530

    Fix CORS for production frontend

--- backend/main.py
+++ backend/main.py
@@ -135,12 +135,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-
- allow_origins=[
+    allow_origins=[
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://fraud-transaction-detection-kj8o.onrender.com"
-],
+    ],
     
     allow_credentials=True,
     allow_methods=["*"],
